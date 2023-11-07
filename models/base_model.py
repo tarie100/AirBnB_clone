bccdd8b48329d3be03ad368d48f7e6a08da686be
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-11-07 14:30:22 -0800

    fixed errors in initialising BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now().isoformat()
         self.updated_at = self.created_at
+
+        if kwargs and '__class__' in kwargs:
+            del kwargs['__class__']
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def update(self):
         self.updated_at = datetime.now().isoformat()
@@ -21,15 +29,6 @@ class BaseModel:
         obj_dict['updated_at'] = self.updated_at
         return obj_dict
 
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-    def __init__(self, *args, **kwargs):
-        #delete the __class__ key if dictionary is not empty
-        if kwargs:
-            del kwargs["__class__"]
-
-
     def dict_saver(self):
         dict_format = {}
         dict_format["__class__"] = self.__class__.__name__
@@ -41,4 +40,4 @@ class BaseModel:
                 return dict_format
     
     def __str__(self):
-        return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
+        return f"{self.__class__.__name__} ({self.id}) {self.__dict__}"
