1516c5a2bc52702e143a68ac41123cb9b7f9a226
tarie100 <tarie.school@gmail.com>
2023-11-08 10:53:58 -0800

    updated console

--- console.py
+++ console.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """import module cmd"""
 import cmd
+import uuid
 """define class"""
 
 class HBNBCommand(cmd.Cmd):
     """assign value to prompt"""
     prompt = '(hbnb) '
 
-    def please_quit(self, args):
+    def do_quit(self, args):
         """Exit program"""
         print("Exiting...!")
         return True
@@ -20,9 +21,6 @@ class HBNBCommand(cmd.Cmd):
     def empty_line(self):
         """Do nothing when an empty line is entered"""
         pass
-
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
 
     def do_create(self, args):
         """creating new instance of BaseModel"""
@@ -44,8 +42,8 @@ if __name__ == '__main__':
         if not args:
             print("** class name missing **")
             return
-        argss = arg.split()
-        class_instance = argss[0]
+        argss = args.split()
+        class_name = argss[0]
 
         if class_name not in self.valid_classes:
             print("** class doesn't exist **")
@@ -61,18 +59,39 @@ if __name__ == '__main__':
         else:
             print("** no instance found **")
 
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name"""
+        if not args:
+            print("** class name missing **")
+            return
+        argss = args.split()
+        class_name = argss[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            if len(argss) < 2:
+                print("** instance id missing **")
+                return
+            instance_id = argss[1]
+            key = "{}.{}".format(class_name, instance_id)
+            if key in models.storage.all():
+                del models.storage.all()[key]
+                models.storage.save()
+            else:
+                print("** no instance found **")
+
     def do_all(self, args):
-            """print string rep of all instances"""
-            argss = args.split()
-            if not args or argss[0] in self.valid_classes:
-                instance_list = []
-                for key, value in models.storage.all().items():
-                    if not args or value.__class__.__name__ == argss[0]:
-                        instance_list.append(str(value))
-                        print(instance_list)
-                    else:
-                        print("** class doesn't exist **")
-    def all_update(self, args):
+        """Prints all string representation of all instances based or not on the class name"""
+        argss = args.split()
+        if not args or argss[0] in self.valid_classes:
+            instance_list = []
+            for key, value in models.storage.all().items():
+                if not args or value.__class__.__name__ == argss[0]:
+                    instance_list.append(str(value))
+                    print(instance_list)
+                else:
+                    print("** class doesn't exist **")
+    
+    def do_update(self, args):
         """update an instance based on the class name and id"""
         if not args:
             print("** class name missing **")
@@ -100,3 +119,5 @@ if __name__ == '__main__':
                 print("** value missing **")
                 return
 
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
