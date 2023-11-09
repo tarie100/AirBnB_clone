4644d95a5cb8c80f750ceec9fec94354746ba653
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-11-09 06:55:00 -0800

    moved given test file to task_test dir

--- task_tests/test_base_model.py
+++ task_tests/test_base_model.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+import unittest
+from datetime import datetime
+from base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBaseModel(unittest.TestCase):
+
+    def test_init(self):
+        base_model = BaseModel()
+        self.assertIsInstance(base_model.id, str)
+        self.assertIsInstance(base_model.created_at, datetime)
+        self.assertIsInstance(base_model.updated_at, datetime)
+
+    def test_save(self):
+        base_model = BaseModel()
+        prev_updated_at = base_model.updated_at
+        base_model.save()
+        self.assertNotEqual(prev_updated_at, base_model.updated_at)
+        self.assertIsInstance(base_model.updated_at, datetime)
+
+    def test_to_dict(self):
+        base_model = BaseModel()
+        obj_dict = base_model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertIsInstance(obj_dict['created_at'], str)
+        self.assertIsInstance(obj_dict['updated_at'], str)
+        self.assertEqual(base_model.id, obj_dict['id'])
+
+    def test_str(self):
+        base_model = BaseModel()
+        expected_str = "[BaseModel] ({}) {}".format(base_model.id, base_model.__dict__)
+        self.assertEqual(str(base_model), expected_str)
+
+if __name__ == '__main__':
+    unittest.main()
